1b8b6d9b90dc801ab83c0d4100eef390527b9a6a
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-01 20:46:11 +0100

    Create collapsible menu for all webpages
    - Create side bar and fill it up with useful links for user actions
    - Using svg, create a icon for opening the side bar
    - Add CSS styling
    - Add some Javascript code to give it the collapsible functionality
    [Delivers #164341780]

---
+++ UI/js/main.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){
+    document.getElementById("side-bar").style.width = '200px';
+  // document.getElementById('epic').style.marginLeft = '250px';
+  }
+
+  function closeSlideMenu(){
+    document.getElementById('side-bar').style.width = '0';
+//      document.getElementById('epic').style.marginLeft = '0';
+  }
