1eedea85fff592dd352e100e43380ad73069f429
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-12 23:49:37 +0100

    Feat(user): Create API endpoints for user signin
    - Add user controllers for signin
    - Add User route
    [Delivers #164523867]

--- Server/routes/users.route.js
+++ Server/routes/users.route.js
@@ -4,5 +4,6 @@ import UserController from '../controllers/user.controller';
 const router = Router();
 
 router.post('/auth/signup', UserController.create);
+router.post('/auth/login', UserController.login);
 
 export default router;
