8c0e32766e6cec388f09d163a7daf72f06820f66
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-13 03:16:47 +0100

    Feat(user): API for getting a single message
    Create controllers
    Create routes
    [Delivers #164590965]

--- Server/routes/route.js
+++ Server/routes/route.js
@@ -8,5 +8,6 @@ router.post('/auth/signup', UserController.create);
 router.post('/auth/login', UserController.login);
 router.post('/messages', MessageController.create);
 router.get('/messages', MessageController.getAll);
+router.get('/messages/:id', MessageController.getOne);
 
 export default router;
