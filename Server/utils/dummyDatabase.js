5bdd34835bcf2be104a450a3ecceac9f34685b7f
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-13 03:23:24 +0100

    Feat(user): API for getting a single message
    - Update database
    [Delivers #164590965]

--- Server/utils/dummyDatabase.js
+++ Server/utils/dummyDatabase.js
@@ -22,4 +22,42 @@ export default {
       password: 'charamie',
     },
   ],
+  messages: [
+    {
+      id: 1,
+      senderId: 3,
+      receiverId: 2,
+      createdOn: new Date().toString(),
+      subject: 'Hello Victor',
+      message: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+      been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+      type and scrambled it to make a type specimen book`,
+      parentMessageId: 1,
+      status: 'draft',
+    },
+    {
+      id: 2,
+      senderId: 2,
+      receiverId: 3,
+      createdOn: new Date().toString(),
+      subject: 'Hello Chidinma',
+      message: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+      been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+      type and scrambled it to make a type specimen book`,
+      parentMessageId: 1,
+      status: 'sent',
+    },
+    {
+      id: 3,
+      senderId: 3,
+      receiverId: 2,
+      createdOn: new Date().toString(),
+      subject: 'Charamie Designs',
+      message: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+      been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+      type and scrambled it to make a type specimen book`,
+      parentMessageId: 1,
+      status: 'draft',
+    },
+  ],
 };
