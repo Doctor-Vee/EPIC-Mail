1d6117b559ccd8176a97665d1906ce7616c8fe95
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-13 03:42:36 +0100

    Bug fix on app.js

--- Server/utils/dummyDatabase.js
+++ Server/utils/dummyDatabase.js
@@ -57,7 +57,7 @@ export default {
       been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
       type and scrambled it to make a type specimen book`,
       parentMessageId: 1,
-      status: 'draft',
+      status: 'read',
     },
   ],
 };
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from 'express';
+// import bodyParser from 'body-parser';
 
 import port from './Server/config/config';
-import routes from './Server/routes/users.route';
+import routes from './Server/routes/route';
 
 
 const app = express();
@@ -12,10 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to ');
+  res.send('Welcome to EPIC Mail');
 });
 
-app.use('/api/v1/users', routes);
+app.use('/api/v1/', routes);
 
 app.listen(PORT, () => { console.log(`App is listening on port ${PORT}`); });
 
