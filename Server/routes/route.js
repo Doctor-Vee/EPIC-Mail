2a9ddaaa4ba68268c0f89315c5f8a1cbb107f8b6
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-14 02:12:03 +0100

    Feat(user): Create middleware for validating input
    - create middleware
    - test on Postman
    [Delivers #164620804]

--- Server/routes/route.js
+++ Server/routes/route.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import UserController from '../controllers/users.controller';
 import MessageController from '../controllers/messages.controller';
+import UserValidator from '../middlewares/users.validator';
 
 const router = Router();
 
-router.post('/auth/signup', UserController.create);
+router.post('/auth/signup',
+  UserValidator.valCreate,
+  UserController.create);
 router.post('/auth/login', UserController.login);
 router.post('/messages', MessageController.create);
 router.get('/messages', MessageController.getAll);
