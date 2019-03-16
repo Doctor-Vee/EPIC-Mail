062a9ed4cc3dee08c47cf43705a0695e34e2f3ba
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-16 21:33:39 +0100

    Feat(user): Edit User Interface
    - Remodel side menu to stay fixed on big screen and disappear only on smaller screens
    [#164679718]

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,9 +1,7 @@
-function openSlideMenu(){
-    document.getElementById("side-bar").style.width = '200px';
-  // document.getElementById('epic').style.marginLeft = '250px';
-  }
+function openSlideMenu() {
+  document.getElementById('side-bar').style.width = '200px';
+}
 
-  function closeSlideMenu(){
-    document.getElementById('side-bar').style.width = '0';
-//      document.getElementById('epic').style.marginLeft = '0';
-  }
+function closeSlideMenu() {
+ document.getElementById('side-bar').style.width = '0';
+}
