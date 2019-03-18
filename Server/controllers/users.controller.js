c8ca9dff46f059f44244ee7308d32571261d8c48
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-03-18 14:11:14 +0100

    Feat(user): Add webtokens and hash users passwords
    - Install dependencies
    - Add necessary code for jsonwebtokens
    - Add necessary code for hashing passwords
    [#164681610]

--- Server/controllers/users.controller.js
+++ Server/controllers/users.controller.js
@@ -1,11 +1,15 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 import UserModel from '../models/users.model';
 
 const UserController = {
   create(req, res) {
     const createdUser = UserModel.create(req.body);
+    const token = jwt.sign({ id: createdUser.id }, process.env.SECRET, { expiresIn: '1h' });
     return res.status(201).send({
       status: 201,
-      data: createdUser,
+      token,
     });
   },
   login(req, res) {
@@ -16,16 +20,32 @@ const UserController = {
         error: 'User does not exist',
       });
     }
-    if (foundUser.password !== req.body.password) {
-      return res.status(401).send({
-        status: 401,
-        error: 'Wrong password',
-      });
-    }
-    return res.status(200).send({
-      status: 200,
-      data: foundUser,
-    });
+
+    bcrypt.compare(req.body.password, foundUser.password)
+      .then((matches) => {
+        if (matches) {
+          const token = jwt.sign({ id: foundUser.id, email: foundUser.email }, process.env.SECRET, { expiresIn: '1h' });
+          return res.status(200).json({
+            status: 'Accepted',
+            token,
+          });
+        }
+        return res.status(401).json({
+          status: 'Wrong password',
+        });
+      })
+      .catch(error => console.log(error));
+    // if (foundUser.password !== req.body.password) {
+    //   return res.status(401).send({
+    //     status: 401,
+    //     error: 'Wrong password',
+    //   });
+    // }
+
+  //   return res.status(200).send({
+  //     status: 200,
+  //     data: foundUser,
+  //   });
   },
 };
 
